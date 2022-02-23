844ec7f2a16acfc125364ea2fdec0f3cc570af58
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-02-23 14:30:17 +1100

    Add win check.

--- script.js
+++ script.js
@@ -35,14 +35,64 @@ const game = (() =>
         let gameBoardArray = [null, null, null, null, null, null, null, null, null];
 
         const _isValidIndex = (index) => {return (index >= 0 && index < 9)};
-        const _indexError = () => {console.log("Invalid index! Only indexes 0-8")};
+        const _indexError = index => {console.log(`${index} is a invalid index! Only indexes 0-8`)};
 
         const getGameBoard = () => {return gameBoardArray};
         const clearGameBoard = () => {gameBoardArray.forEach(square => square = null)};
-        const getSquare = (index) => {if(_isValidIndex(index)) { return gameBoardArray[index]} else {_indexError()}};
-        const setSquare = (index, marker) => {if(_isValidIndex(index)) {gameBoardArray[index] = marker; return gameBoardArray[index]} else {_indexError()}};
+        const getSquare = (index) => {if(_isValidIndex(index)) { return gameBoardArray[index]} else {_indexError(index)}};
+        const setSquare = (index, marker) => {if(_isValidIndex(index)) {gameBoardArray[index] = marker; return gameBoardArray[index]} else {_indexError(index)}};
+        const checkRow = (marker, square) =>
+        {
+            square = +square;
 
-        return {getGameBoard, clearGameBoard, getSquare, setSquare};
+            //Check row
+            switch(square%3)
+            {
+                case 0:
+                    if(getSquare(square+1) == marker && getSquare(square+2) == marker)
+                    return true;
+                break;
+                case 1:
+                    if(getSquare(square-1) == marker && getSquare(square+1) == marker)
+                    return true;
+                break;
+                case 2:
+                    if(getSquare(square-1) == marker && getSquare(square-2) == marker)
+                    return true;
+                break;                    
+            }
+            //check column
+            switch(Math.floor(square/3))
+            {
+                case 0:
+                    if(getSquare(square+3) == marker && getSquare(square+6) == marker)
+                    return true;
+                break;
+                case 1:
+                    if(getSquare(square-3) == marker && getSquare(square+3) == marker)
+                    return true;
+                break;
+                case 2:
+                    if(getSquare(square-3) == marker && getSquare(square-6) == marker)
+                    return true;
+                break;
+            }
+            //check diagonals
+            if(getSquare(4) == marker)
+            {
+                if(getSquare(0) == marker && getSquare(8) == marker)
+                {
+                    return true;
+                }
+                else if(getGameBoard(2) == marker && getSquare(6) == marker)
+                {
+                    return true;
+                }
+            }            
+            return false;
+        };
+
+        return {getGameBoard, clearGameBoard, getSquare, setSquare, checkRow};
     })();
 
     const _playerFactory = ((name, marker) =>
@@ -58,23 +108,37 @@ const game = (() =>
 
     let turn = playerX.getMarker();
 
-    const getTurn = () =>
+    const _getTurn = () =>
     {
         return turn;
     }
 
-    const nextTurn = () =>
+    const _nextTurn = () =>
     {
         turn = turn == playerX.getMarker() ? playerO.getMarker() : playerX.getMarker();
     }
+
+    const _checkWin = square =>
+    {
+        if(gameBoard.checkRow(_getTurn(), square))
+        {
+            console.log(`Player ${_getTurn()} has won.`);
+        }
+        else
+        {
+            console.log(`Player ${_getTurn()} has not won yet.`);
+        }
+    }
+
 
     const input = (squareIndex) =>
     {
         console.log('CLICK!')
         if(gameBoard.getSquare(squareIndex) == null)
         {
-            gameBoard.setSquare(squareIndex, getTurn());
-            nextTurn();
+            gameBoard.setSquare(squareIndex, _getTurn());
+            _checkWin(squareIndex);
+            _nextTurn();
             displayController.displayBoard(gameBoard.getGameBoard());
         }
         else
