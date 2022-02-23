ba9e53d72d0c0fd28d4de4d6bfac8ec042c177ab
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-02-23 11:38:03 +1100

    Update index.html with main-container div. Update script.js with displayController module which interacts with DOM. Display controller can delete all elements and generate squares using a gameBoard array. Add input and turn functions to game module.

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-const Game = (() => {
-    const GameBoard = (() => 
-    {
-    let gameBoard = [null, null, null, null, null, null, null, null, null];
+const gameBoard = (() => 
+{
+    let gameBoardArray = [null, null, null, null, null, null, null, null, null];
 
     const _isValidIndex = (index) => {return (index >= 0 && index < 9)};
     const _indexError = () => {console.log("Invalid index! Only indexes 0-8")};
 
-    const getGameBoard = () => {return gameBoard};
-    const clearGameBoard = () => {gameBoard.forEach(square => square = null)};
-    const getSquare = (index) => {if(_isValidIndex(index)) { return gameBoard[index]} else {_indexError()}};
-    const setSquare = (index, marker) => {if(_isValidIndex(index)) {gameBoard[index] = marker; return gameBoard[index]} else {_indexError()}};
+    const getGameBoard = () => {return gameBoardArray};
+    const clearGameBoard = () => {gameBoardArray.forEach(square => square = null)};
+    const getSquare = (index) => {if(_isValidIndex(index)) { return gameBoardArray[index]} else {_indexError()}};
+    const setSquare = (index, marker) => {if(_isValidIndex(index)) {gameBoardArray[index] = marker; return gameBoardArray[index]} else {_indexError()}};
 
     return {getGameBoard, clearGameBoard, getSquare, setSquare};
-    })();
+})();
 
+const displayController = (() =>
+{
+    const mainContainer = document.querySelector('.main-container');
+    
+    const _clearDisplay = () => {
+        const squares = document.querySelectorAll('.square > *');
+
+        squares.forEach(square =>
+        {
+            mainContainer.removeChild(square);
+        });
+    }
+
+    const displayBoard = gameBoard => {
+        _clearDisplay();
+
+        gameBoard.forEach((square, i) =>
+        {
+            const squareElement = document.createElement('div');
+            squareElement.classList.add('square');
+            squareElement.textContent = square;
+            squareElement.setAttribute('data-square', i);
+            squareElement.addEventListener('click', e => game.input(squareElement.getAttribute('data-square')));
+            mainContainer.appendChild(squareElement);
+        });
+    }
+
+    return {displayBoard};
+})();
+
+const game = (() => 
+{
     const _playerFactory = ((name, marker) =>
     {
         const getName = () => {return name};
@@ -25,10 +56,36 @@ const Game = (() => {
     const playerX = _playerFactory("Player X", "X");
     const playerO = _playerFactory("Player O", "O");
 
+    let turn = 'X';
 
-    return {playerX, playerO};
+    const getTurn = () =>
+    {
+        return turn;
+    }
+
+    const nextTurn = () =>
+    {
+        turn = turn == 'X' ? 'O' : 'X';
+    }
+
+    const input = (squareIndex) =>
+    {
+        if(getSquare(squareIndex) == null)
+        {
+            setSquare(squareIndex, getTurn());
+            nextTurn();
+            displayController.displayBoard(gameBoard.getGameBoard());
+        }
+        else
+        {
+            console.log("Tile is already marked!");
+        }
+    }
+
+    displayController.displayBoard(gameBoard.getGameBoard());
+
+    return {getTurn, nextTurn, input};
 })();
-
 
 
 
